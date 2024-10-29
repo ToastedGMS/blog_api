65668bba80b6b1d45f9669cd51ba579b299cdf04
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-10-29 15:14:52 -0300

    add functions to create, read and delete refresh tokens

--- backend/models/prisma/script.js
+++ backend/models/prisma/script.js
@@ -1,7 +1,6 @@
 const bcrypt = require('bcryptjs');
-const prisma = require('../..');
 
-async function addUserToDatabase(email, password, name) {
+async function addUserToDatabase(email, password, name, prisma) {
 	const hashedPassword = await bcrypt.hash(password, 10);
 
 	try {
@@ -16,4 +15,57 @@ async function addUserToDatabase(email, password, name) {
 	}
 }
 
-module.exports = { addUserToDatabase };
+async function saveRefreshToken(token, email, prisma) {
+	try {
+		const newToken = await prisma.refreshTokens.create({
+			data: { token, userEmail: email },
+		});
+
+		return newToken;
+	} catch (error) {
+		console.error('Error adding token to database', error);
+		throw error;
+	}
+}
+
+async function checkToken(refreshToken, prisma) {
+	try {
+		const token = await prisma.refreshTokens.findFirst({
+			where: { token: refreshToken },
+		});
+
+		return token !== null;
+	} catch (error) {
+		console.error('Error checking token in database', error);
+		throw error;
+	}
+}
+
+async function removeToken(email, prisma) {
+	try {
+		console.log(`Attempting to remove token for email: ${email}`);
+
+		const tokenToDelete = await prisma.refreshTokens.findFirst({
+			where: { userEmail: email },
+		});
+
+		if (tokenToDelete) {
+			await prisma.refreshTokens.delete({
+				where: { id: tokenToDelete.id },
+			});
+			console.log('Token removed successfully');
+		} else {
+			console.log('Token not found');
+		}
+	} catch (error) {
+		console.error('Error removing token from database', error);
+		throw error;
+	}
+}
+
+module.exports = {
+	addUserToDatabase,
+	saveRefreshToken,
+	checkToken,
+	removeToken,
+};
