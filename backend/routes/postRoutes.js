116e215aff9fe2b5b4ac5485213235f4792ce43a
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-11-05 19:58:51 -0300

    move comment related routes to separate file

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -5,13 +5,6 @@ const {
 	routerDeletePost,
 	putPost,
 } = require('../controllers/postController');
-
-const {
-	postComment,
-	getComment,
-	putComment,
-	routerDeleteComment,
-} = require('../controllers/commentController');
 const { authenticateToken } = require('../controllers/tokenController');
 
 router.post('/new', authenticateToken, postPost);
@@ -21,17 +14,5 @@ router.get('/', getPost);
 router.delete('/:postId', authenticateToken, routerDeletePost);
 
 router.put('/:postId', authenticateToken, putPost);
-
-router.post('/:postId/comments', authenticateToken, postComment);
-
-router.get('/:postId/comments', getComment);
-
-router.delete(
-	'/:postId/comments/:commentId',
-	authenticateToken,
-	routerDeleteComment
-);
-
-router.put('/:postId/comments/:commentId', authenticateToken, putComment);
 
 module.exports = router;
