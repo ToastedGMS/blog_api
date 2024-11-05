86b003fe579e12b7dace8d126f6bd201a51e37f8
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-11-05 19:56:48 -0300

    add comment and like related router files

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 //server related imports
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
 const port = 3000;
 
@@ -12,13 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 //routes
 const postRouter = require('./routes/postRoutes');
 app.use('/posts', postRouter);
-// no comment route file was created due to overlapping route parameters in nested routers.
-// instead comment routes were placed in post route file, separating only the controller file.
+const commentRouter = require('./routes/commentRoutes');
+app.use('/posts', commentRouter);
+const likeRouter = require('./routes/likeRoutes');
+app.use('/posts', likeRouter);
 const tokenRouter = require('./routes/tokenRoutes');
 app.use('/tokens', tokenRouter);
 const userRouter = require('./routes/userRoutes');
 app.use('/users', userRouter);
 
+//server listener
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
 });
