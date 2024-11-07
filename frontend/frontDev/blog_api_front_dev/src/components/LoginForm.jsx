68a5dcdd6f14fdb884fbbc80531de97d533792ed
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-11-07 19:11:23 -0300

    create login form

--- frontend/frontDev/blog_api_front_dev/src/components/LoginForm.jsx
+++ frontend/frontDev/blog_api_front_dev/src/components/LoginForm.jsx
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/LoginForm.css';
 
 function LoginForm() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			const response = await fetch('http://localhost:3000/users/login', {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json',
+				},
+				body: JSON.stringify({ email, password }),
+			});
+
+			const data = await response.json();
+
+			if (response.ok) {
+				console.log('Login successful', data);
+			} else {
+				console.error('Login failed:', data);
+			}
+		} catch (error) {
+			console.error('Error during login:', error);
+		}
+	};
+
 	return (
 		<>
 			<h1>Login</h1>
-			<form action="http://localhost:3000/users/login" method="post">
+			<form onSubmit={handleSubmit}>
 				<label htmlFor="email">Email:</label>
-				<input type="email" name="email" id="email" />
+				<input
+					type="email"
+					name="email"
+					id="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					required
+				/>
 
-				<label htmlFor="password">Password: </label>
-				<input type="password" name="password" id="password" />
+				<label htmlFor="password">Password:</label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					required
+				/>
 
-				<button type="submit">Log in</button>
+				<button>Login</button>
 			</form>
 		</>
 	);
