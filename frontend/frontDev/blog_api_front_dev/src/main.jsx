516fa4c82665e5564c2f593225a85849d49f6134
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-11-07 20:26:31 -0300

    add logout route

--- frontend/frontDev/blog_api_front_dev/src/main.jsx
+++ frontend/frontDev/blog_api_front_dev/src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App.jsx';
 import LoginForm from './components/LoginForm.jsx';
+import Logout from './components/Logout.jsx';
 
 const router = createBrowserRouter([
 	{ path: '/', element: <App /> },
 	{
 		path: '/login',
 		element: <LoginForm />,
+	},
+	{
+		path: '/logout',
+		element: <Logout />,
 	},
 ]);
 
